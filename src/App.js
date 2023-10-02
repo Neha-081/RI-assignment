7c178327b7bb9c970a3b2e263cf3c3a989bed28e
Neha-081 <nehaprajapati11502gmai.com>
2023-10-02 23:32:06 +0530

    build landing page with no records found

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import noRecordsFound from '../src/assets/no-records.svg';
 
 function App() {
   return (
     <div className="App">
-     RI- Assignment 
+    <header>
+        <nav className="navbar">
+            <div className="container">
+                <p>Employee List</p>
+            </div>
+        </nav>
+    </header>
+    <main>
+       <img className='no-records' src={noRecordsFound} alt="no-records-img" />
+       <button className='plus-button'>+</button>
+    </main>
     </div>
   );
 }
